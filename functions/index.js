d3ed1f617868faeaa30349ac42892658399677d5
Manuel Mendivil <manuel.mendivil@dsindigo.com>
2019-04-25 14:28:48 -0500

    add tgf, friends, commonFollowers endpoints

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,23 @@ const functions = require('firebase-functions');
 const getCommonFollowers = require('./helpers/getCommonFollowers')
 const getFriends = require('./helpers/getFriends')
 const getCommonFriends = require('./helpers/getCommonFriends')
-const TGFtext = require('./helpers/TGFtext')
+const TGFObject = require('./helpers/TGFObject')
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.followers = functions.https.onRequest((request, response) => {
+exports.commonFollowers = functions.https.onRequest((request, response) => {
   const users = request.query.usuarios
   const [ userA, userB ] = users.split(',')
+
+  // if (!userA || !userB) return response.send('Error. Usuarios requeridos en querystring "usuarios"')
+
+  return getCommonFollowers(userA, userB).then(common => response.send(common))
+});
+
+exports.friends = functions.https.onRequest((request, response) => {
+  const users = request.query.usuarios
+  const [ userA, userB ] = users.split(',')
+
+  // if (!userA || !userB) return response.send('Error. Usuarios requeridos en querystring "usuarios"')
 
   let commonFollowers,
     friendsA, friendsB,
@@ -33,10 +37,34 @@ exports.followers = functions.https.onRequest((request, response) => {
     })
     .then(([ commonFollowers, friendsA, friendsB ]) => {
       const commonFriends = getCommonFriends(commonFollowers, friendsA, friendsB)
-      console.log('Followers >>> \n', commonFollowers, '\n')
-      console.log('Friends >>', commonFriends, '\n')
-      // TGFtext(userA, userB, commonFriends)
       return response.send(commonFriends);
     })
 
 });
+
+exports.tgf = functions.https.onRequest((request, response) => {
+  const users = request.query.usuarios
+  const [ userA, userB ] = users.split(',')
+
+  // if (!userA || !userB) return response.send('Error. Usuarios requeridos en querystring "usuarios"')
+
+  let commonFollowers,
+    friendsA, friendsB,
+    commonFriends
+
+  return Promise
+    .all([
+      getCommonFollowers(userA, userB),
+      getFriends(userA, userB)
+    ])
+    .then(([ common, friends ]) => {
+      commonFollowers = common
+      const [ friendsA, friendsB ] = friends
+      return [ commonFollowers, friendsA, friendsB ]
+    })
+    .then(([ commonFollowers, friendsA, friendsB ]) => {
+      const commonFriends = getCommonFriends(commonFollowers, friendsA, friendsB)
+      const TGF = TGFObject(userA, userB, commonFriends)
+      return response.send(TGF)
+    })
+})
