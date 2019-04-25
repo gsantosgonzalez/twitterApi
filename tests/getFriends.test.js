236e89c7d27d834481087ff1289ac6c9c4edfa0c
Manuel Mendivil <manuel.mendivil@dsindigo.com>
2019-04-25 15:52:59 -0500

    add common followers and friends test

--- tests/getFriends.test.js
+++ tests/getFriends.test.js
@@ -1,24 +1,33 @@
 import test from 'ava'
-const apiMock = {
-  get: function () {
-    return  Promise.resolve({
-      data: {
-        users: [
-         { screen_name: 'I3OI2I236O' },
-         { screen_name: 'energizatesolar' },
-         { screen_name: 'LabCDMX' },
-         { screen_name: 'GaelDz' },
-         { screen_name: 'alexcceea' }
-      ]
-     }   
-    })
-  }
-}
-
 const proxyquire = require('proxyquire')
-const getFriends = proxyquire('../functions/helpers/getFriends', { '../api': apiMock })
+const getFriends = proxyquire('../functions/helpers/getFriends', { '../api': { get: {} } })
 
-const friends = [
+const friendsMockA = Promise.resolve({
+  data: {
+    users: [
+      { screen_name: 'I3OI2I236O' },
+      { screen_name: 'energizatesolar' },
+    ]
+  }   
+})
+const friendsMockB = Promise.resolve({
+  data: {
+    users: [
+      { screen_name: 'I3OI2I236O' },
+      { screen_name: 'energizatesolar' },
+      { screen_name: 'LabCDMX' },
+      { screen_name: 'GaelDz' },
+      { screen_name: 'alexcceea' }
+    ]
+  }   
+})
+
+const friendsA = [
+  'I3OI2I236O',
+  'energizatesolar',
+]
+
+const friendsB = [
   'I3OI2I236O',
   'energizatesolar',
   'LabCDMX',
@@ -27,9 +36,14 @@ const friends = [
 ]
 
 test('get friends', async t => {
-  const o = await getFriends('userA', 'userB')
+  const o = await getFriends(
+    'userA',
+    'userB',
+    [ friendsMockA, friendsMockB ]
+  )
+
   t.deepEqual(o, [
-    friends,
-    friends
+    friendsA,
+    friendsB
   ])
 })
