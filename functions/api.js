fddaa7369f797fe3bf9d126e52af6ea8f2d5bdf5
Manuel Mendivil <manuel.mendivil@dsindigo.com>
2019-04-25 16:14:28 -0500

    fix rest cors

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,16 @@
 const Twit = require('twit')
 
-const T = new Twit({
-  consumer_key: '4LKphBbf3077nBlliAMNzpQXM',
-  consumer_secret: 'pXH43u3KvRwwzrz16K9NOTm60jmhn4TlnSBBnqxTBxJIgtUShV',
-  access_token: '985236639814504449-j7ulHEQ5dKPmZvGjcagrXuITXCL8rpu',
-  access_token_secret: '7qS4LuJ3iJ6XZO5lW1cDTV3280ckRv7lOsLmgHpJYnM3i',
-})
 // const T = new Twit({
-  // consumer_key: 'Ja9PY1Q6N3RIXEa7U90gYslH0',
-  // consumer_secret: 'STrcxaWAsfmAu83nclUNGamzHDhSimbJCdQKf46MIHEcVj8o2V',
-  // access_token: '985236639814504449-giR1gUD08VzZA6feMUiwNBVVCXWnOJZ',
-  // access_token_secret: 'HSPnNMfC3PRq1xjyTAMjyvuXS6GoUOHNq5EXO0fDMDAqL',
+  // consumer_key: '4LKphBbf3077nBlliAMNzpQXM',
+  // consumer_secret: 'pXH43u3KvRwwzrz16K9NOTm60jmhn4TlnSBBnqxTBxJIgtUShV',
+  // access_token: '985236639814504449-j7ulHEQ5dKPmZvGjcagrXuITXCL8rpu',
+  // access_token_secret: '7qS4LuJ3iJ6XZO5lW1cDTV3280ckRv7lOsLmgHpJYnM3i',
 // })
+const T = new Twit({
+  consumer_key: 'Ja9PY1Q6N3RIXEa7U90gYslH0',
+  consumer_secret: 'STrcxaWAsfmAu83nclUNGamzHDhSimbJCdQKf46MIHEcVj8o2V',
+  access_token: '985236639814504449-giR1gUD08VzZA6feMUiwNBVVCXWnOJZ',
+  access_token_secret: 'HSPnNMfC3PRq1xjyTAMjyvuXS6GoUOHNq5EXO0fDMDAqL',
+})
 
 module.exports = T
